55a8b6a5b673866ae0f99ed67dfe3dd6c14ef3b6
Tobias Heinzle <tobias.heinzle@hotmail.de>
2024-01-02 22:59:56 +0100

    ADD lichess bot support

--- lichess-bot.py
+++ lichess-bot.py
@@ -1,12 +1,13 @@
 from requests_oauthlib import OAuth2Session
 import asyncio
 import berserk
+import threading
 import chess
 from chess import engine
 from time import time, sleep
 
 
-limit = 10
+limit = 1
 board = chess.Board()
 
 async def analyze_position(position: chess.Board, engine: engine.Protocol, time_limit_seconds: float = 0.1) -> (str, dict):
@@ -24,106 +25,95 @@ async def analyze_position(position: chess.Board, engine: engine.Protocol, time_
         result = analysis.info
     
     return (bestmove.move, result)
-        
 
-
-# async def main() -> None:
-#     transport, engine = await chess.engine.popen_uci("./target/release/rust_chess")
-
-#     board = chess.Board()
-#     while not board.is_game_over():
-#          move, result = await analyze_position(board, engine, limit)
-#          board.push(move)
-#          print(move, result)
-#          print(board)
-        
-
-#     print(chess.Board().variation_san(board.move_stack))
-
-#     await engine.quit()
-
-# asyncio.run(main())
-
-# quit()
-
-
+token = ""
 with open('./lichess.token') as f:
     token = f.read()
 
-
-# session = berserk.TokenSession(token)
-
-session = OAuth2Session(...)
-
+session = berserk.TokenSession(token)
 client = berserk.Client(session)
 
-client.account.upgrade_to_bot()
-
-class Game(threading.Thread):
-
-    def __init__(self, client, game_id, **kwargs):
-
-        super().__init__(**kwargs)
-
-        self.game_id = game_id
-
-        self.client = client
-
-        self.stream = client.bots.stream_game_state(game_id)
-
-        self.current_state = next(self.stream)
-
-        self.board = chess.Board()
-
-
-    async def run(self):
-        transport, engine = await chess.engine.popen_uci("./target/release/rust_chess")
-
-        for event in self.stream:
-
-            if event['type'] == 'gameState':
-
-                await self.handle_state_change(event)
-
-            elif event['type'] == 'chatLine':
-
-                self.handle_chat_line(event)
-
-        await engine.quit()
-
-
-    async def handle_state_change(self, game_state):
-        move, result = await analyze_position(self.board, self.engine, limit)
-        self.board.push(move)
-        print("ID: " + str(self.game_id) + " info: " + str(move) + " " + str(result))
-        pass
-
-
-    def handle_chat_line(self, chat_line):
-        print("ID: " + str(self.game_id) + " Chat: " + str(chat_line))
-        pass
-
-
 def should_accept(challenge_event) -> bool:
-    return True
+    with open("allowed.challengers") as file:
 
-def main_loop():
+        allowed_challengers = file.readlines()
+        if challenge_event["challenge"]["challenger"]["id"] in allowed_challengers:
+            return True
+        return False
+    
+async def main_loop():
     is_polite = True
 
     for event in client.bots.stream_incoming_events():
 
         if event['type'] == 'challenge':
-
             if should_accept(event):
-
-                client.bots.accept_challenge(event['id'])
+                print("Accepted challenge of " + event["challenge"]["challenger"]["id"])
+                client.bots.accept_challenge(event["challenge"]['id'])
 
             elif is_polite:
 
-                client.bots.decline_challenge(event['id'])
+                client.bots.decline_challenge(event["challenge"]['id'])
 
         elif event['type'] == 'gameStart':
+            print(event)
 
-            game = Game(event['id'])
+            game_id = event["game"]["gameId"]
+            color = event["game"]['color']
+            fen = event["game"]["fen"]
 
-            game.start()
+            await play_game(client,  game_id, color, fen)
+
+async def bot_move(board, engine, client, game_id):
+    move, result = await analyze_position(board, engine, limit)
+
+    client.bots.make_move(game_id, str(move))
+
+    print("ID: " + str(game_id) + " info: " + str(move) + " " + str(result))
+
+
+async def play_game(client, game_id, color, fen):
+    stream = client.bots.stream_game_state(game_id)
+    current_state = next(stream)
+
+    print(current_state)
+
+    board = chess.Board()
+    board.set_fen(fen)
+    print("game started!")
+
+    skip_value = 0
+    transport, engine = await chess.engine.popen_uci("./target/release/rust_chess")
+
+    if color == "white":
+        skip_value = 1
+        await bot_move(board, engine, client, game_id)
+
+
+    for event in stream:
+        print(event)
+
+        if event['type'] == 'gameState':
+            if event['status'] != 'started':
+                break
+
+            board = chess.Board()
+
+            moves_played = event['moves'].split(" ")
+
+            if len(moves_played) % 2 == skip_value:
+                continue
+
+            for move in moves_played:
+                board.push(chess.Move.from_uci(move))
+
+            await bot_move(board, engine, client, game_id)
+
+    await engine.quit()
+
+
+            
+
+asyncio.run(main_loop())
+
+quit()
